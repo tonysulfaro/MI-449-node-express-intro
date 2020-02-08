beac452ad2ad1fa55a35bd6c5ab988e798d21084
Tony Sulfaro <tony116523@gmail.com>
2020-02-08 18:45:07 -0500

    can link to static files (css). header unresolved

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 var express = require('express')
 var app = express()
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 var port = process.env.PORT || 8080
 
+var articles = {}
+
+function createArticle (article) {
+  var id = Object.keys(articles).length
+  article.createdAt = new Date()
+  articles[id] = article
+}
+
+createArticle({
+  title: 'Squirrel escapes squirrel prison, now at large!',
+  content: 'Yesterday evening at 7:47PM, the criminal squirrel Rabid Rabindra got out of his cage.'
+})
+createArticle({
+  title: 'Squirrel lured back into prison with peanut butter',
+  content: 'This morning at 10:13PM, the smell of peanut butter drew Rabid Rabindra the squirrel back into his cage.'
+})
+
 app.get('/', function (request, response) {
-  response.render('pages/index')
+  response.render('pages/index', {
+    articles: articles
+  })
 })
 
 app.get('/about', function (request, response) {
